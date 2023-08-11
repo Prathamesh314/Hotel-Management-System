018734ada663766b623e25da1ca230f3adcb36dc
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-08-11 10:02:22 +0530

    added eureka server and client

---
+++ Discovery-Server/src/test/java/com/kurve/discoverserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurve.discoverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
